0cb8e3913816843039151d30992029bd088efaff
Jeff Blaisdell <jeff.blaisdell@gmail.com>
2015-06-10 10:07:46 -0500

    asdf

--- grails-app/domain/com/smartthings/model/Book.groovy
+++ grails-app/domain/com/smartthings/model/Book.groovy
@@ -26,4 +26,26 @@ class Book {
         isbn = lastIsbn ? lastIsbn + 'a' : 'a'
     }
 
+    /**
+
+    // This works.
+    // During test, the createCriteria implementation differs from
+    // the implementation used during the actual running of app.
+    // The test implementation flushes before executing query.
+    // The flush causes hibernate events to fire which result in circular
+    // execution and ultimately a StackOverflowException.
+    // Using withNewSession isolates the execution preventing the circular behavior.
+
+    void beforeInsert() {
+        withNewSession {
+            def lastIsbn = createCriteria().get {
+                projections {
+                    max 'isbn'
+                }
+            }
+            isbn = lastIsbn ? lastIsbn + 'a' : 'a'
+        }
+    }
+
+    */
 }
